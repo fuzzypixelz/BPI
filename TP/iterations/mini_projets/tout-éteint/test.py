2fd6a3c8a9b7440f856ef522f6715248d77eda73
fuzzypixelz <fuzzypixelz@localhost.localdomain>
2020-10-09 23:07:27 +0200

    Minor changes

--- TP/iterations/mini_projets/tout-éteint/test.py
+++
@@ -1,6 +0,0 @@
-text = "Here's a piece of text I want to overwrite"
-repl = "BALEETED!" # What we want to write
-print(text, end='\r') # Write the text and return
-print('hello')
-#print(f'{repl: <{len(text)}}')
-
